9be71e408bb375a8af8539ea6b0d9f117a7dd09b
stwe <stegowe@gmail.com>
2013-09-10 20:27:20 +0200

    update file header

--- Column/ActionColumn.php
+++ Column/ActionColumn.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace Sg\DatatablesBundle\Column;
+/**
+ * This file is part of the SgDatatablesBundle package.
+ *
+ * (c) stwe <https://github.com/stwe/DatatablesBundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-/*
-* This file is part of the SgDatatablesBundle package.
-*
-* (c) stwe <https://github.com/stwe/DatatablesBundle>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+namespace Sg\DatatablesBundle\Column;
 
 use Sg\DatatablesBundle\Column\AbstractColumn as BaseColumn;
 
--- Twig/DatatableTwigExtension.php
+++ Twig/DatatableTwigExtension.php
@@ -1,13 +1,13 @@
 <?php
 
-/*
-* This file is part of the SgDatatablesBundle package.
-*
-* (c) stwe <https://github.com/stwe/DatatablesBundle>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+/**
+ * This file is part of the SgDatatablesBundle package.
+ *
+ * (c) stwe <https://github.com/stwe/DatatablesBundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Sg\DatatablesBundle\Twig;
 
