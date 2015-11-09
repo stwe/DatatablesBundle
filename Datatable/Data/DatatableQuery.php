1114541d472e6214035fe540f461ac70ceb08ec8
Celvin Rivas <josecelvin@gmail.com>
2015-11-09 11:30:06 -0600

    adding multiple schema compatibility
    Working with complex DB, sometimes are dived into many schemas, by example.
    
    /**
     * catpersonal
     * @ORM\Table(name="secondschema.catpersonal")
     * @ORM\Entity()
     */
    class catpersonal
    
    I got the error:
          Syntax Error] line 0, col 31: Error: Expected Doctrine\ORM\Query\Lexer::T_OPEN_CURLY_BRACE, got 'catpersonal'
    
    but with this fix everything is working well if you are using multiple schema or not.

--- Datatable/Data/DatatableQuery.php
+++ Datatable/Data/DatatableQuery.php
@@ -167,7 +167,15 @@ class DatatableQuery
         $this->entity = $this->datatableView->getEntity();
         $this->em = $this->datatableView->getEntityManager();
         $this->metadata = $this->getMetadata($this->entity);
-        $this->tableName = $this->getTableName($this->metadata);
+        
+        $keyName=$this->getTableName($metadata);
+        if(strpos($keyName, ".")){
+            $tableName = substr($keyName, strpos($keyName, ".") + 1);
+            $this->tableName = $tableName;
+        }else{
+            $this->tableName =$keyName;
+        }
+        
         $this->rootEntityIdentifier = $this->getIdentifier($this->metadata);
         $this->qb = $this->em->createQueryBuilder();
 
@@ -404,7 +412,14 @@ class DatatableQuery
     private function setSelectFrom()
     {
         foreach ($this->selectColumns as $key => $value) {
-            $this->qb->addSelect('partial ' . $key . '.{' . implode(',', $this->selectColumns[$key]) . '}');
+            //$this->qb->addSelect('partial ' . $key . '.{' . implode(',', $this->selectColumns[$key]) . '}');
+            if(strpos($key, ".")){
+                $tableName = substr($key, strpos($key, ".") + 1);
+            }else{
+                $tableName=$key;
+            }
+            
+            $this->qb->addSelect('partial ' . $tableName . '.{' . implode(',', $this->selectColumns[$key]) . '}');
         }
 
         $this->qb->from($this->entity, $this->tableName);
