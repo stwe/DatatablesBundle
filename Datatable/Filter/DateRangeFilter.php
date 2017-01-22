99f297367d977c5ec063f92c17a563c9272b7b18
stwe <ursus.maritimus2.0@gmail.com>
2017-01-22 17:08:03 +0100

    daterange filter added

---
+++ Datatable/Filter/DateRangeFilter.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of the SgDatatablesBundle package.
+ *
+ * (c) stwe <https://github.com/stwe/DatatablesBundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sg\DatatablesBundle\Datatable\Filter;
+
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Query\Expr\Andx;
+use DateTime;
+
+/**
+ * Class DateRangeFilter
+ *
+ * @package Sg\DatatablesBundle\Datatable\Filter
+ */
+class DateRangeFilter extends AbstractFilter
+{
+    //-------------------------------------------------
+    // FilterInterface
+    //-------------------------------------------------
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplate()
+    {
+        return 'SgDatatablesBundle:filter:daterange.html.twig';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addAndExpression(Andx $andExpr, QueryBuilder $qb, $searchField, $searchValue, &$parameterCounter)
+    {
+        list($_dateStart, $_dateEnd) = explode(' - ', $searchValue);
+        $dateStart = new DateTime($_dateStart);
+        $dateEnd = new DateTime($_dateEnd);
+        $dateEnd->setTime(23, 59, 59);
+
+        $andExpr = $this->getBetweenAndExpression($andExpr, $qb, $searchField, $dateStart->format('Y-m-d H:i:s'), $dateEnd->format('Y-m-d H:i:s'), $parameterCounter);
+        $parameterCounter += 2;
+
+        return $andExpr;
+    }
+
+    //-------------------------------------------------
+    // Helper
+    //-------------------------------------------------
+
+    /**
+     * Returns the type for the <input> element.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return 'text';
+    }
+}
