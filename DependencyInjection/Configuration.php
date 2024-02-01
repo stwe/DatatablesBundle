661d4fefe8a3f5cccdace3b4eec192ee3b67564f
stephan <stephan@vierkant.net>
2024-02-01 15:16:28 +0100

    TreeBuilder as return type

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('sg_datatables');
         if (method_exists($treeBuilder, 'getRootNode')) {
